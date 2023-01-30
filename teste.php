0f911c3933dad22f26cddcb24219763ca73dbf17
rsvaamonde <123279592+rsvaamonde@users.noreply.github.com>
2023-01-30 15:09:16 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<title>Teste da Linguagem PHP</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php 
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Robson Vaamonde<br>';
+				echo 'Linkedin: linkedin.com/in/robson-vaamonde-0b029028/<br>';
+				echo 'Site: procedimentosemti.com.br<br>';
+				echo 'Facebook: facebook.com/ProcedimentosEmTI<br>';
+				echo 'Facebook: facebook.com/BoraParaPratica<br>';
+				echo 'Instagram: instagram.com/procedimentoem/<br>';
+				echo 'YouTube: youtube.com/BoraParaPratica<br>'; 
+			?>
+		</body>
+	</html>
